81efe3fcf5dfa92ce1e161fba4bec8a1d2171f78
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2022-04-12 17:04:18 +0530

    Harveen | Adding Timestamp information

--- infer.py
+++ infer.py
@@ -24,7 +24,7 @@ class Wav2vecHF:
 
         return processor, model.to(self.device)
 
-    def transcribe(self, wav_path):                
+    def transcribe(self, wav_path, hotwords = [], return_timestamps = False):                
         
         audio_input, sample_rate = sf.read(wav_path)
 
@@ -36,15 +36,31 @@ class Wav2vecHF:
         if self.lm == 'viterbi':
             text = self.processor.batch_decode(torch.argmax(logits, dim=-1), skip_special_tokens=True)
         elif self.lm == 'kenlm':
-            result = self.processor.batch_decode(logits.cpu().numpy())
+            result = self.processor.batch_decode(logits.cpu().numpy(), hotwords = hotwords, output_word_offsets=return_timestamps)
             text = result.text
-        
-        return text
+            
+            if return_timestamps:
+                time_offset = self.model.config.inputs_to_logits_ratio / self.processor.feature_extractor.sampling_rate
+                word_offsets = [
+                    {
+                        "word": d["word"],
+                        "start_time": round(d["start_offset"] * time_offset, 2),
+                        "end_time": round(d["end_offset"] * time_offset, 2),
+                    }
+                    for d in result.word_offsets[0]
+                ]
+                
+                return text[0], word_offsets
+            
+        return text[0]
 
-    def transcribe_dir(self, audio_dir):
+    def transcribe_dir(self, audio_dir, hotwords = [], return_timestamps = False):
         wav_files = glob.glob(audio_dir + '/*.wav')
         for wav_file in tqdm(wav_files):
-            text = self.transcribe(wav_file)
+            text = self.transcribe(wav_file, hotwords, return_timestamps)
             filename = wav_file.split('/')[-1].replace('.wav', '.txt')
             with open(audio_dir + '/' + filename, mode='w+', encoding='utf-8') as ofile:
                 ofile.write(text)
+
+print(Wav2vecHF('Harveenchadha/hindi_model_with_lm_vakyansh', 'kenlm').transcribe('/home/harveen/blindtest_442338.wav',  return_timestamps = True))
+
